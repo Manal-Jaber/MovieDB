f41c318f9c3476a830277216f698b5c22c0d4303
Manal-Jaber <manal.jaber94@gmail.com>
2020-10-21 15:37:43 +0300

    step 3

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+let date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let time = hours + ":" + minutes;
 app.get('/', (req, res) => res.send('ok'))
+
+app.get('/test', (req, res) => res.status(200).send('ok'))
+
+app.get('/time', (req, res) => res.status(200).send(`${time}`))
 
 app.listen(port)
