4f13c38a8dae270104dbe4c5d1dffc8888cb1403
Manal-Jaber <manal.jaber94@gmail.com>
2020-10-22 12:07:13 +0300

    step 5

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ let minutes = date.getMinutes();
 let time = hours + ":" + minutes;
 let data, error;
 
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
 app.get('/', (req, res) => res.send('ok'))
 
 app.get('/test', (req, res) => res.status(200).send('ok'))
@@ -26,4 +33,25 @@ data = req.query.s;
   console.error(`error: ${error}`)
 }
 });
+
+app.get('/movies/add', (req, res) => {
+  res.status().send();
+  data = req.params;
+  });
+
+app.get('/movies/get', (req, res) => {
+  res.status(200).send(movies);
+  data = movies;
+});
+
+app.get('/movies/edit', (req, res) => {
+  res.status().send();
+  data = req.params;
+});
+
+app.get('/movies/delete', (req, res) => {
+  res.status().send();
+  data = req.params;
+});
+
 app.listen(port)
